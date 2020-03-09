a5c1351ad7b073e3201b1dd0ed4cedf03431cb70
o7Ghost <33361213+o7Ghost@users.noreply.github.com>
2020-03-08 17:25:11 -0700

    Add able to display list of Presidential Candidates in Home page

--- USVoteTracker/Controllers/HomeController.cs
+++ USVoteTracker/Controllers/HomeController.cs
@@ -1,19 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 using USVoteTracker.Models;
+using System.Diagnostics;
 
 namespace USVoteTracker.Controllers
 {
     public class HomeController : Controller
     {
-        private List<Candidate> Candidates;
-        public Candidate C = new Candidate();
+        ObjectCache cache = MemoryCache.Default;
+        List<Candidate> Candidates;
+        
+        public HomeController()
+        {
+            Candidates = cache["Candidates"] as List<Candidate>;
+
+            if (Candidates == null)
+            {
+                Candidates = new List<Candidate>();
+            }
+        }
+
+        public void saveCache()
+        {
+            cache["Candidates"] = Candidates;
+        }
+
         public ActionResult Index()
         {
-            return View(C);
+            //System.Diagnostics.Debug.WriteLine(Candidates.Count());
+            return View(Candidates);
         }
 
         public ActionResult About()
@@ -32,11 +51,15 @@ namespace USVoteTracker.Controllers
 
         public ActionResult AddCandidate(string Name, string Party)
         {
-            
+            Candidate C = new Candidate();
             C.Name = Name;
             C.Party = Party;
             C.Votes = 0;
 
+            Candidates.Add(C);
+
+            saveCache();
+     
             return RedirectToAction("Index");
         }
 
